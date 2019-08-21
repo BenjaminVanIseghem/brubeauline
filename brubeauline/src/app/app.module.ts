b6770cf25770f0d9868606f92bb4b113baaf2e9e
Benjamin Van Iseghem <benjamin.vaniseghem.w3891@student.hogent.be>
2019-08-21 20:39:00 +0200

    1i8n method with locales

--- brubeauline/src/app/app.module.ts
+++ brubeauline/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { InfoComponent } from './info/info.component';
+import { BruggeComponent } from './brugge/brugge.component';
+import { RecensiesComponent } from './recensies/recensies.component';
+import { ContactComponent } from './contact/contact.component';
+
+const appRoutes: Routes = [
+  {path: 'info', component: InfoComponent},
+  {path: 'brugge', component: BruggeComponent},
+  {path: 'recensies', component: RecensiesComponent},
+  {path: 'contact', component: ContactComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    BruggeComponent,
+    RecensiesComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
